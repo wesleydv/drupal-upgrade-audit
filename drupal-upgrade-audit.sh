d35613fa2c9a62623f6abf9c481cc246fe13cbcc
Wesley De Vrient <wesley@digital54.com>
2021-07-15 21:07:28 +0200

    Add complexity and check if drupal-check is available

--- drupal-upgrade-audit.sh
+++ drupal-upgrade-audit.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if ! command -v drupal-check &> /dev/null
+then
+    echo "drupal-check command not found, please install it globally https://github.com/mglaman/drupal-check#installation"
+    exit 1
+fi
+
 # Clone repo in /tmp folder if needed
-echo "### Checking out the code ###"
+echo "### Cloning the code, you know like Dolly ###"
 if [ -d "/tmp/$1" ]
 then
 	cd /tmp/$1
@@ -23,6 +29,12 @@ else
         echo "Could not find any deprecated code in custom modules, you may still want to check /tmp/$1-check.txt"
 fi
 
+# Complexity
+echo "### Assessing complexity  ###"
+CUSTOM_MODULES=`find docroot/modules/custom -type f -name *.info.yml | wc -l | xargs`
+LINES_CODE=`find docroot/modules/custom -type f \( -name "*.php" -o -name "*.module" \) -print0 | xargs -0 cat | wc -l | xargs`
+echo "There are $CUSTOM_MODULES custom modules and $LINES_CODE lines of custom code."
+
 # Get 10th commit.
 echo "### Looking for the original Drupal version ###"
 REV=`git log --reverse --format=%H | head -n 10 | tail -n 1`
@@ -37,3 +49,5 @@ fi
 VERSION=`sed -n "s/  const VERSION = '//p" $DRUPAL_PHP | sed -n "s/';//p"`
 echo "Orginal Drupal version: $VERSION"
 
+# Go back to master to avoid confusion
+git checkout --quiet master
